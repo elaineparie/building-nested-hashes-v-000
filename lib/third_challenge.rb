5ff8694250453314c19c15a827db2ab8dfc2ea6a
Elaine Anderson <eparie@bu.edu>
2018-05-25 16:37:23 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,9 +12,9 @@ def third_challenge
         },
       :hero_friends => [ {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Mercutio", age: "18", attitude: "hot-headed"}
-        
+
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {
         {name: "Lord Capulet", age: "50"}
@@ -27,10 +27,10 @@ def third_challenge
         },
       :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},
           {name: "Nurse", age: "44", attitude: "worried"}
-        
+
       ]
    }
   }
 
-  
+
 end
